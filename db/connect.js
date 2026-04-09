816a60ab3c6f8e2c1104025d9cfd26565ea112b9
Fawsi Ahmed Mohamed Abdi <Fowcode101@gmail.com>
2026-04-09 15:28:24 +0200

    ´fejl fix

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,14 @@
 import pg from "pg";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 const env = {
-    host: process.env.PG_HOST,
-    port: parseInt(process.env.PG_PORT),
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    ssl: { rejectUnauthorized: false },
+  host: process.env.PG_HOST,
+  port: parseInt(process.env.PG_PORT),
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  ssl: { rejectUnauthorized: false },
 };
 
 export async function connect() {

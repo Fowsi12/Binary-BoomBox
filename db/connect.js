8b780534d1fd0bfe5c8f776fb03de6e3c37a1900
Fawsi Ahmed Mohamed Abdi <Fowcode101@gmail.com>
2026-04-16 13:44:26 +0200

    Big Update 3.0! .env fix
    Co-authored-by: Funato-san <Funato-san@users.noreply.github.com>
    Co-authored-by: Faysal Faraah <fayzf0028@gmail.com>

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,33 @@
 import pg from "pg";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
+
 const env = {
-    host: process.env.PG_HOST,
-    port: parseInt(process.env.PG_PORT),
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    ssl: { rejectUnauthorized: false },
+  host: process.env.PG_HOST,
+  port: parseInt(process.env.PG_PORT),
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  ssl: { rejectUnauthorized: false },
+};
+
+const env1 = {
+  host: process.env.PG_HOST1,
+  port: parseInt(process.env.PG_PORT1),
+  database: process.env.PG_DATABASE1,
+  user: process.env.PG_USER1,
+  password: process.env.PG_PASSWORD1,
+  ssl: { rejectUnauthorized: false },
+};
+
+const env2 = {
+  host: process.env.PG_HOST2,
+  port: parseInt(process.env.PG_PORT2),
+  database: process.env.PG_DATABASE2,
+  user: process.env.PG_USER2,
+  password: process.env.PG_PASSWORD2,
+  ssl: { rejectUnauthorized: false },
 };
 
 export async function connect() {
